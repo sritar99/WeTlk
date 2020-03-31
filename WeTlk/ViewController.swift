f285d3bca87985ee6445c1120697f442314b6360
sritar99 <sadhutarun00@gmail.com>
2020-04-01 02:11:40 +0530

    Initial Commit

---
+++ WeTlk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeTlk
+//
+//  Created by Sri Harsha on 01/04/20.
+//  Copyright © 2020 Sri Harsha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
