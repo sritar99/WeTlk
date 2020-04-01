6a5e3da8c9a13003a1b3f0a3820e48ddeb84d8bf
sritar99 <sadhutarun00@gmail.com>
2020-04-01 11:00:08 +0530

    ScreenBoards and Controllers are constructed

--- WeTlk/Controllers/WelcomeViewController.swift
+++ WeTlk/Controllers/WelcomeViewController.swift
@@ -8,13 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var titleLabel: UILabel!
+    
 }
 
