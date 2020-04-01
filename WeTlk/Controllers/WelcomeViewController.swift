41aa80318d236a7e25773a7e018d89961a4c186b
sritar99 <sadhutarun00@gmail.com>
2020-04-01 22:41:41 +0530

    Navigation Bar View is Modified , improvements are made

--- WeTlk/Controllers/WelcomeViewController.swift
+++ WeTlk/Controllers/WelcomeViewController.swift
@@ -9,6 +9,16 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
